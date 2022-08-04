7a80dc7e0a6aec82400f6ace251ccdc6dc371458
Adam Due Hansen <adamduehan@gmail.com>
2022-08-04 07:00:53 +0200

    Adds icon to dark mode button.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import BinaryToDecimal from './binary-to-demical/BinaryToDecimal';
 import TabPanel from './components/tabs/TabPanel';
 import Tabs from './components/tabs/Tabs';
 import DecimalToBinary from './decimal-to-binary/DecimalToBinary';
 
 function App(): JSX.Element {
+  const darkModeButtonSvgRef = useRef<SVGSVGElement>(null);
+
   useEffect(() => {
     if (
       localStorage.getItem('theme') === 'dark' ||
@@ -12,12 +14,14 @@ function App(): JSX.Element {
         window.matchMedia('(prefers-color-scheme: dark)').matches)
     ) {
       document.documentElement.classList.add('dark');
+      darkModeButtonSvgRef.current!.classList.add('fill-white');
     }
   }, []);
 
   function toggleDarkMode() {
     const classAdded = document.documentElement.classList.toggle('dark');
     localStorage.setItem('theme', classAdded ? 'dark' : 'light');
+    darkModeButtonSvgRef.current!.classList.toggle('fill-white');
   }
 
   return (
@@ -37,7 +41,20 @@ function App(): JSX.Element {
           </TabPanel>
         </Tabs>
       </div>
-      <button onClick={toggleDarkMode}>Toggle</button>
+      <button onClick={toggleDarkMode} className='fixed bottom-2 right-2'>
+        <svg
+          version='1.1'
+          xmlns='http://www.w3.org/2000/svg'
+          width='32'
+          height='32'
+          viewBox='0 0 32 32'
+          ref={darkModeButtonSvgRef}
+          className='fill-white'
+        >
+          <title>brightness-contrast</title>
+          <path d='M16 8c-4.418 0-8 3.582-8 8s3.582 8 8 8 8-3.582 8-8-3.582-8-8-8zM16 21v-10c2.757 0 5 2.243 5 5s-2.243 5-5 5zM16 26c1.105 0 2 0.895 2 2v2c0 1.105-0.895 2-2 2s-2-0.895-2-2v-2c0-1.105 0.895-2 2-2zM16 6c-1.105 0-2-0.895-2-2v-2c0-1.105 0.895-2 2-2s2 0.895 2 2v2c0 1.105-0.895 2-2 2zM30 14c1.105 0 2 0.895 2 2s-0.895 2-2 2h-2c-1.105 0-2-0.895-2-2s0.895-2 2-2h2zM6 16c0 1.105-0.895 2-2 2h-2c-1.105 0-2-0.895-2-2s0.895-2 2-2h2c1.105 0 2 0.895 2 2zM25.899 23.071l1.414 1.414c0.781 0.781 0.781 2.047 0 2.828s-2.047 0.781-2.828 0l-1.414-1.414c-0.781-0.781-0.781-2.047 0-2.828s2.047-0.781 2.828 0zM6.101 8.929l-1.414-1.414c-0.781-0.781-0.781-2.047 0-2.828s2.047-0.781 2.828 0l1.414 1.414c0.781 0.781 0.781 2.047 0 2.828s-2.047 0.781-2.828 0zM25.899 8.929c-0.781 0.781-2.047 0.781-2.828 0s-0.781-2.047 0-2.828l1.414-1.414c0.781-0.781 2.047-0.781 2.828 0s0.781 2.047 0 2.828l-1.414 1.414zM6.101 23.071c0.781-0.781 2.047-0.781 2.828 0s0.781 2.047 0 2.828l-1.414 1.414c-0.781 0.781-2.047 0.781-2.828 0s-0.781-2.047 0-2.828l1.414-1.414z'></path>
+        </svg>
+      </button>
     </main>
   );
 }
