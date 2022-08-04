8247b2bd136feaba1b01017a6849ee74a78eb030
Adam Due Hansen <adamduehan@gmail.com>
2022-08-04 06:45:18 +0200

    Dark theme can be toggled on button click.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { useEffect } from 'react';
 import BinaryToDecimal from './binary-to-demical/BinaryToDecimal';
 import TabPanel from './components/tabs/TabPanel';
 import Tabs from './components/tabs/Tabs';
 import DecimalToBinary from './decimal-to-binary/DecimalToBinary';
 
 function App(): JSX.Element {
+  useEffect(() => {
+    if (
+      localStorage.getItem('theme') === 'dark' ||
+      (localStorage.getItem('theme') === null &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
+      document.documentElement.classList.add('dark');
+    }
+  }, []);
+
+  function toggleDarkMode() {
+    const classAdded = document.documentElement.classList.toggle('dark');
+    localStorage.setItem('theme', classAdded ? 'dark' : 'light');
+  }
+
   return (
     <main className='p-2 md:h-screen md:w-screen md:flex md:justify-center'>
       <div className='md:w-[500px]'>
@@ -21,6 +37,7 @@ function App(): JSX.Element {
           </TabPanel>
         </Tabs>
       </div>
+      <button onClick={toggleDarkMode}>Toggle</button>
     </main>
   );
 }
